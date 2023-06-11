2cae3876ce9b0df1258a846ae5349ab15e93c647
Shinymimiq <shinymimiq@gmail.com>
2023-06-12 02:54:15 +0800

    First commit of the config file.

--- src/templating.rs
+++ src/templating.rs
@@ -1,6 +1,11 @@
 use tera::{Context, Tera};
 use crate::MarkdownFile;
 
+use std::fs;
+use toml;
+
+// At the beginning of your file
+const DEFAULT_TEMPLATE_PATH: &str = "templates/**/*";
 /// Renders a vector of Markdown files using Tera templates and returns a vector of rendered HTML strings.
 ///
 /// # Arguments
@@ -11,7 +16,27 @@ use crate::MarkdownFile;
 ///
 /// A vector of rendered HTML strings.
 pub fn render_files(files: Vec<MarkdownFile>) -> Vec<String> {
-    let tera: Tera = match Tera::new("templates/**/*") {
+    // Read the configuration file
+    let config: String = fs::read_to_string("config/config.toml")
+    .expect("Could not read configuration file");
+
+    // Parse the configuration file
+    let value = config.parse::<toml::Value>()
+    .expect("Could not parse configuration file");
+
+    let template_path: String = value["templates"]["path"]
+    .as_str()
+    .and_then(|path| {
+        let user_template_path = format!("{}/**/*", path);
+        if fs::metadata(&user_template_path).is_ok() {
+            Some(user_template_path)
+        } else {
+            None
+        }
+    })
+    .unwrap_or_else(|| DEFAULT_TEMPLATE_PATH.to_string());
+
+    let tera: Tera = match Tera::new(&template_path) {
         Ok(t) => t,
         Err(e) => {
             println!("Parsing error(s): {}", e);
